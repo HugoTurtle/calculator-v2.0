15a312f8078d757268480cee94a1d6fff36fd2e0
Hugo Perez <hugopereztm12309@gmail.com>
2022-06-28 11:25:51 -0400

    Add an operate function

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ let add = (a,b) => a + b;
 let subtract = (a,b) => a - b;
 let multiply = (a,b) => a * b;
 let divide = (a,b) => a / b;
+
+let operate = (operator,a,b) => {
+    switch(operator) {
+        case add : return add(a,b);
+        case subtract : return subtract(a,b);
+        case multiply : return multiply(a,b);
+        case divide : return divide(a,b);
+    }
+}
